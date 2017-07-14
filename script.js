49b8ae6950207bc16a3c2a48d94316e0b5a73c4f
James <boydston.j@gmail.com>
2017-07-14 15:08:45 -0700

    js input and output

--- script.js
+++ script.js
@@ -1,45 +1,11 @@
 console.log('Week 3 Project');
 
-// Button IDs
-// #displayB
-let displayB = getElementById('displayB')
-// #number0B
-let number0B = getElementById('number0B')
-// #number1B
-let number1B = getElementById('number1B')
-// #number2B
-let number2B = getElementById('number2B')
-// #number3B
-let number3B = getElementById('number3B')
-// #number4B
-let number4B = getElementById('number4B')
-// #number5B
-let number5B = getElementById('number5B')
-// #number6B
-let number6B = getElementById('number6B')
-// #number7B
-let number7B = getElementById('number7B')
-// #number8B
-let number8B = getElementById('number8B')
-// #number9B
-let number9B = getElementById('number9B')
-// #operatorDecB
-let operatorDecB = getElementById('operatorDecB')
-// #operatorDivB
-let operatorDivB = getElementById('operatorDivB')
-// #operatorMultB
-let operatorMultB = getElementById('operatorMultB')
-// #operatorAddB
-let operatorAddB = getElementById('operatorAddB')
-// #operatorSubB
-let operatorSubB = getElementById('operatorSubB')
-// #operatorEquB
-let operatorEquB = getElementById('operatorEquB')
-// #operatorClearB
-let operatorClearB = getElementById('operatorClearB')
-// #operatorPastB
-let operatorPastB = getElementById('operatorPastB')
-// #operatorModuloB
-let operatorModuloB = getElementById('operatorModuloB')
-// #operatorSQRtB
-let operatorSQRtB = getElementById('operatorSQRtB')
+// #calcMain
+let input = document.getElementById('calcMain')
+
+let displayB = document.getElementById('displayB')
+
+input.addEventListener('click', function(e) {
+  let output = e.target.innerHTML
+  displayB.innerHTML += output
+})
