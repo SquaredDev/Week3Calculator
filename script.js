b947e1b33e549f18f1e122fff6d332b0835a9c70
James <boydston.j@gmail.com>
2017-07-16 02:07:34 -0700

    many inprovements

--- script.js
+++ script.js
@@ -4,22 +4,37 @@ console.log('Week 3 Project');
 let input = document.getElementById('calcMain')
 
 let displayB = document.getElementById('displayB')
-let clear = document.getElementById('operatorClearB')
+
+let hLog = 'test'
 
 input.addEventListener('click', function(e) {
   let output = e.target.innerHTML
   displayB.innerHTML += output
 
-  let history = document.getElementById('operatorPastB')
   let buffer = ''
   let equal = document.getElementById('operatorEquB')
-
   equal.addEventListener('click', function (e) {
+    hLog += displayB.innerHTML + '\n' + ' is '
     buffer = eval(displayB.innerHTML)
     displayB.innerHTML = buffer
   })
 
+  let sqrt = document.getElementById('operatorSQRtB')
+  sqrt.addEventListener('click', function (e) {
+    hLog += 'Square Root of ' + displayB.innerHTML + ' is '
+    buffer = Math.sqrt(displayB.innerHTML)
+    displayB.innerHTML = buffer
+  })
+
+  let clear = document.getElementById('operatorClearB')
   clear.addEventListener('click', function wipe() {
+    hLog += displayB.innerHTML + '\n'
     displayB.innerHTML = ''
   })
 })
+
+// let history = document.getElementById('operatorPastB')
+// history.addEventListener('click', function (e) {
+//   console.log(hLog)
+//   hLog = ''
+// })
