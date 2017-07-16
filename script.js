e98256a6be99b6a8ad25c6bae5bb9b578b12ac49
James <boydston.j@gmail.com>
2017-07-16 03:13:39 -0700

    tweaks

--- script.js
+++ script.js
@@ -5,13 +5,12 @@ let input = document.getElementById('calcMain')
 
 let displayB = document.getElementById('displayB')
 
-let hLog = 'test'
 
 input.addEventListener('click', function(e) {
   let output = e.target.innerHTML
   displayB.innerHTML += output
 
-  let buffer = ''
+  let buffer = ' '
   let equal = document.getElementById('operatorEquB')
   equal.addEventListener('click', function (e) {
     hLog += displayB.innerHTML + '\n' + ' is '
@@ -29,12 +28,12 @@ input.addEventListener('click', function(e) {
   let clear = document.getElementById('operatorClearB')
   clear.addEventListener('click', function wipe() {
     hLog += displayB.innerHTML + '\n'
-    displayB.innerHTML = ''
+    displayB.innerHTML = ' '
   })
+  // let hLog = 'test'
+  //
+  // let history = document.getElementById('operatorPastB'){
+  //   hLog = ''
+  //   history.innerHTML = hLog
+  // }
 })
-
-// let history = document.getElementById('operatorPastB')
-// history.addEventListener('click', function (e) {
-//   console.log(hLog)
-//   hLog = ''
-// })
