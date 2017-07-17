a4d3935153c3265de72ece34bb7d102b2fed09c9
James <boydston.j@gmail.com>
2017-07-17 08:40:59 -0700

    weekend work

--- script.js
+++ script.js
@@ -2,33 +2,32 @@ console.log('Week 3 Project');
 
 // #calcMain
 let input = document.getElementById('calcMain')
-
 let displayB = document.getElementById('displayB')
-
 
 input.addEventListener('click', function(e) {
   let output = e.target.innerHTML
   displayB.innerHTML += output
 
-  let buffer = ' '
+  let buffer = ''
   let equal = document.getElementById('operatorEquB')
   equal.addEventListener('click', function (e) {
-    hLog += displayB.innerHTML + '\n' + ' is '
+    // hLog += displayB.innerHTML + '\n' + ' is '
     buffer = eval(displayB.innerHTML)
     displayB.innerHTML = buffer
   })
 
-  let sqrt = document.getElementById('operatorSQRtB')
-  sqrt.addEventListener('click', function (e) {
-    hLog += 'Square Root of ' + displayB.innerHTML + ' is '
+  let sqrtB = document.getElementById('operatorSQRtB')
+  sqrtB.addEventListener('click', function (e) {
+    // hLog += 'Square Root of ' + displayB.innerHTML + ' is '
+    buffer = eval(displayB.innerHTML)
     buffer = Math.sqrt(displayB.innerHTML)
     displayB.innerHTML = buffer
   })
 
   let clear = document.getElementById('operatorClearB')
   clear.addEventListener('click', function wipe() {
-    hLog += displayB.innerHTML + '\n'
-    displayB.innerHTML = ' '
+    // hLog += displayB.innerHTML + '\n'
+    displayB.innerHTML = ''
   })
   // let hLog = 'test'
   //
